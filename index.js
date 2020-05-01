cf53d695ec735da64feef60a196c92942424a023
Lautaro Osácar <lautaro.eo@gmail.com>
2020-05-01 13:16:04 -0300

    aadsds

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get("/isworking", function (req, res) {
   res.send("VERCEL ANDA?");
 });
 
+app.get("/", function (req, res) {
+  res.send("HOLAA");
+});
+
 app.listen(3000, function () {
   console.log("RUNNING ON PORT 3000");
 });
