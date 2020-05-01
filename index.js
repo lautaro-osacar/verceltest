f19cfc242ce5a5778eaf1d1d8e8b7eb2c1e64131
Lautaro Osácar <lautaro.eo@gmail.com>
2020-05-01 12:52:49 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+
+app.get("/isworking", function (req, res) {
+  res.send("VERCEL ANDA");
+});
+
+app.listen(3000, function () {
+  console.log("RUNNING ON PORT 3000");
+});
